72878231af66c99ce2c1a0399e70e44eb4d18876
kev <kevin.nouhouayi@epitech.eu>
2026-09-04 20:39:55 +0100

    fix: correction du build (voir historique prive Epitech)

--- tri.c
+++ tri.c
@@ -7,25 +7,6 @@
 
 #include "my.h"
 
-void permute(list_t **l1, list_t **l2)
-{
-    list_t *temp = NULL;
-
-    temp = *l1;
-    *l1 = *l2;
-    *l2 = temp;
-}
-void q_sort_name1(list_t **T, tri_t *t, int start, int end)
-{
-    while (comp1(T[t->i], T[t->pivot]) == 1 && t->i < end)
-        t->i++;
-    while (comp1(T[t->j], T[t->pivot]) == 0 && t->j > start)
-        t->j--;
-    if (t->i < t->j) {
-        permute(&(T[t->i]), &(T[t->j]));
-    }
-}
-
 void q_sort_name(list_t **T, int start, int end)
 {
     tri_t t;
@@ -40,5 +21,39 @@ void q_sort_name(list_t **T, int start, int end)
         permute(&(T[t.j]), &(T[t.pivot]));
         q_sort_name(T, start, t.j - 1);
         q_sort_name(T, t.j + 1, end);
+    }
+}
+
+void q_sort_type(list_t **T, int start, int end)
+{
+    tri_t t;
+
+    if (start < end) {
+        t.pivot = start;
+        t.i = start;
+        t.j = end;
+        while (t.i < t.j) {
+            q_sort_type2(T, start, end, &t);
+        }
+        permute(&(T[t.j]), &(T[t.pivot]));
+        q_sort_type(T, start, t.j - 1);
+        q_sort_type(T, t.j + 1, end);
+    }
+}
+
+void q_sort_id(list_t **T, int start, int end)
+{
+    tri_t t;
+
+    if (start < end) {
+        t.pivot = start;
+        t.i = start;
+        t.j = end;
+        while (t.i < t.j) {
+            q_sort_id2(T, start, end, &t);
+        }
+        permute(&(T[t.j]), &(T[t.pivot]));
+        q_sort_id(T, start, t.j - 1);
+        q_sort_id(T, t.j + 1, end);
     }
 }
